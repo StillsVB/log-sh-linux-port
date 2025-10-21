e85026c4a233a9ed0c50cb2f05f0f2d2a7f0b9f0
StillsVB <stillsinvestments@outlook.com>
2025-10-21 03:35:07 -0700

    Use global install, remove PATH append

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 mkdir -p ~/bin logs/general projects
 curl -sSL https://raw.githubusercontent.com/StillsVB/log-sh-linux-port/main/log.sh -o ~/bin/log.sh
 chmod +x ~/bin/log.sh
-echo 'export PATH="$HOME/bin:$PATH"' >> ~/.profile
-source ~/.profile
+sudo cp ~/bin/log.sh /usr/local/bin/log
 echo "🎉 log.sh installed! Run: log"
