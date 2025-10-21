c3651681e1020727ca898f1fbf0583fcf8fe2d4c
StillsVB <stillsinvestments@outlook.com>
2025-10-20 22:13:03 -0700

    Fix setup.sh repo URL

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p ~/bin logs/general projects
-curl -sSL https://raw.githubusercontent.com/good1/log-sh/main/log.sh -o ~/bin/log.sh
+curl -sSL https://raw.githubusercontent.com/StillsVB/log-sh-linux-port/main/log.sh -o ~/bin/log.sh
 chmod +x ~/bin/log.sh
 echo 'export PATH="$HOME/bin:$PATH"' >> ~/.profile
+source ~/.profile
 echo "🎉 log.sh installed! Run: log"
